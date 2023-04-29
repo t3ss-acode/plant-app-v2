25934598986cfee12e61e80216015e6b1e93f7cf
t3ss-acode <t3ss.ankar@gmail.com>
2023-04-29 13:37:56 +0200

    can only allow letters

--- lib/screens/add_plant.dart
+++ lib/screens/add_plant.dart
@@ -17,41 +17,41 @@ class _AddPlantScreen extends State<AddPlantScreen> {
   Widget build(BuildContext context) {
     return ScaffoldWithAppBar(
       title: 'Add plant',
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              const SizedBox(height: 24),
-              const GenericTextFormField(
-                labelText: 'Name',
-              ),
-              const SizedBox(height: 24),
-              const GenericTextFormField(
-                labelText: 'Days between reminder',
-              ),
-              const SizedBox(height: 24),
-              const GenericTextFormFieldTime(
-                labelText: 'Time of reminder',
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 24.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Validate returns true if the form is valid, or false otherwise.
-                    if (_formKey.currentState!.validate()) {
-                      // If the form is valid, display a snackbar. In the real world,
-                      // you'd often call a server or save the information in a database.
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Processing Data')),
-                      );
-                    }
-                  },
-                  child: const Text('Submit'),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const SizedBox(height: 24),
+                const GenericTextFormField(
+                  labelText: 'Name',
                 ),
-              ),
-            ],
+                const SizedBox(height: 24),
+                const GenericTextFormField(
+                  labelText: 'Days between reminder',
+                  numbersInput: true,
+                ),
+                const SizedBox(height: 24),
+                const GenericTextFormFieldTime(
+                  labelText: 'Time of reminder',
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Validate returns true if the form is valid, or false otherwise.
+                      if (_formKey.currentState!.validate()) {
+                        // Close keyboard and show loader while plant is being created
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/text_form_fields/generic_text_form_field.dart
+++ lib/widgets/text_form_fields/generic_text_form_field.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class GenericTextFormField extends StatelessWidget {
   final String labelText;
+  final bool numbersInput;
 
   const GenericTextFormField({
     super.key,
     required this.labelText,
+    this.numbersInput = false,
   });
 
   @override
@@ -18,6 +21,10 @@ class GenericTextFormField extends StatelessWidget {
         labelText: labelText,
         errorStyle: const TextStyle(fontSize: 16),
       ),
+      keyboardType: numbersInput ? TextInputType.number : TextInputType.text,
+      inputFormatters: numbersInput
+          ? <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly]
+          : <TextInputFormatter>[],
       // The validator receives the text that the user has entered.
       validator: (value) {
         if (value == null || value.isEmpty) {
